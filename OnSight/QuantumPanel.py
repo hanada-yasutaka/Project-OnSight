31e183b117dc81853e225d6197f85f42b4eb6471
Yasutaka Hanada <hanada1985@gmail.com>
2011-08-18 14:44:17 +0900

    Fixed Draw Event

--- OnSight/QuantumPanel.py
+++ OnSight/QuantumPanel.py
@@ -28,10 +28,15 @@ class QuantumPanel(_SubPanel):
         self.state = 'p'
         self.step = wx.xrc.XRCCTRL(self.panel, 'SpinCtrlDrawStep').GetValue()
         self.logplot = False
+        self.cmap_data = []
+        exp_part = int(wx.xrc.XRCCTRL(self.panel, 'TextCtrlLogMin').GetValue())
+        self.vmin = 10**(-exp_part)
+        self.draw = self.DrawHsm
+        self.rep = 'hsm'
         
-        print self.map.Para.para
         def OnApply(event):
             self.Initialization()
+
         def OnRadioBoxInitial(event):
             radio = event.GetSelection()
             if radio == 0 or radio == 3:
@@ -47,38 +52,85 @@ class QuantumPanel(_SubPanel):
                 self.state = 'cs'
                 wx.xrc.XRCCTRL(self.panel, 'TextCtrlq_c').Enable(True)
                 wx.xrc.XRCCTRL(self.panel, 'TextCtrlp_c').Enable(True)
+
         def OnSetInitialState(event):
+            self.plotpanel.clear()
             self.range = self.get_range()
             self.ini_c = self.get_initial()
             self.qmap.setRange(self.range[0], self.range[1], self.range[2],self.range[3], self.range[4])
+            
             if self.state in ('p','plt'):
                 self.qmap.setState(self.state,self.ini_c[1])
             elif self.state in ('q'):
                 self.qmap.setState(self.state, self.ini_c[0])
             else:
                 self.qmap.setState(self.state, self.ini_c[0],self.ini_c[1])
-            self.Draw(iter=0)
+            self.DrawHsm(0, 'hsm')
+
         def OnSpinIteration(event):
             self.iteration = wx.xrc.XRCCTRL(self.panel,'SpinCtrlIteration').GetValue()
+
         def OnButtonIteration(event):
             self.qmap.evolve(self.iteration)
+            self.qmap.get_pvecs()
+
         def OnRadioBoxRep(event):
             radio = event.GetSelection()
-            print radio
+            if radio == 0:
+                self.rep = 'hsm'
+                self.draw = self.DrawHsm
+            elif radio == 1:
+                self.rep = 'p'
+                self.draw = self.Draw
+            else:
+                self.rep = 'q'
+                self.draw = self.Draw
+
         def OnSpinDrawStep(event):
             self.step = wx.xrc.XRCCTRL(self.panel, 'SpinCtrlDrawStep').GetValue()
+            if self.step == self.iteration:
+                wx.xrc.XRCCTRL(self.panel,'ButtonNext').Enable(False)
+            elif self.step == 0:
+                wx.xrc.XRCCTRL(self.panel,'ButtonPrev').Enable(False)
+            else:
+                wx.xrc.XRCCTRL(self.panel,'ButtonPrev').Enable(True)
+                wx.xrc.XRCCTRL(self.panel,'ButtonNext').Enable(True)
+            wx.xrc.XRCCTRL(self.panel,'SpinCtrlDrawStep').SetRange(0,self.iteration)
+
         def OnButtonDraw(event):
-            self.Draw(self.step)
+            self.plotpanel.clear()
+            self.draw(self.step, self.rep)
+
         def OnButtonNext(event):
             self.step += 1
             wx.xrc.XRCCTRL(self.panel,'SpinCtrlDrawStep').SetValue(self.step)
-            self.Draw(self.step)
+            if self.step == self.iteration:
+                wx.xrc.XRCCTRL(self.panel,'ButtonNext').Enable(False)
+            else:
+                wx.xrc.XRCCTRL(self.panel,'ButtonPrev').Enable(True)
+                wx.xrc.XRCCTRL(self.panel,'ButtonNext').Enable(True)
+            self.draw(self.step, self.rep)
+
         def OnButtonPrev(event):
             self.step -= 1
             wx.xrc.XRCCTRL(self.panel,'SpinCtrlDrawStep').SetValue(self.step)
-            self.Draw(self.step)
+            if self.step == 0:
+                wx.xrc.XRCCTRL(self.panel,'ButtonPrev').Enable(False)
+            else:
+                wx.xrc.XRCCTRL(self.panel,'ButtonPrev').Enable(True)
+                wx.xrc.XRCCTRL(self.panel,'ButtonNext').Enable(True)
+
+            self.draw(self.step, self.rep)
+
         def OnCheckBoxLog(event):
             self.logplot = event.IsChecked()
+ 
+        def OnTextLogMin(event):
+            exp_part = wx.xrc.XRCCTRL(self.panel, 'TextCtrlLogMin').GetValue()
+            if exp_part != '':
+                self.vmin = 10**(-int(exp_part))
+
+
 
 
         self.Bind(wx.EVT_BUTTON, OnApply, wx.xrc.XRCCTRL(self.panel, 'ButtonApply'))
@@ -98,14 +150,15 @@ class QuantumPanel(_SubPanel):
         self.Bind(wx.EVT_BUTTON, OnButtonNext, wx.xrc.XRCCTRL(self.panel, 'ButtonNext'))
         self.Bind(wx.EVT_BUTTON, OnButtonPrev, wx.xrc.XRCCTRL(self.panel, 'ButtonPrev'))
         self.Bind(wx.EVT_CHECKBOX, OnCheckBoxLog, wx.xrc.XRCCTRL(self.panel, 'CheckBoxLog'))
-            
+        self.Bind(wx.EVT_TEXT, OnTextLogMin, wx.xrc.XRCCTRL(self.panel, 'TextCtrlLogMin'))
         ## make plotpanel
         self.plotpanel=parent.GetParent().MakePlotPanel(self.title)
         self.plotpanel.OnPress = self.OnPress
+        self.plotpanel.plot()
+        self.set_lim()
+        self.plotpanel.draw()
+
     def Initialization(self):
-        self.map = self.mapsystem.map
-        self.qmap = maps.QuantumMap.QMap(self.map)
-        
         self.range = self.get_range()
         self.vrange = self.get_vrange()
         self.ini_c = self.get_initial()
@@ -114,18 +167,30 @@ class QuantumPanel(_SubPanel):
         self.state = 'p'
         self.step = wx.xrc.XRCCTRL(self.panel, 'SpinCtrlDrawStep').GetValue()
         self.logplot = False
+        self.cmap_data = []
+        exp_part = int(wx.xrc.XRCCTRL(self.panel, 'TextCtrlLogMin').GetValue())
+        self.vmin = 10**(-exp_part)
+        self.draw = self.DrawHsm
+        self.rep = 'hsm'
         
-        print self.map.Para.para
+    def set_lim(self):
+        range = self.get_vrange()
+        self.plotpanel.xlim = (range[0], range[1])
+        self.plotpanel.ylim = (range[2], range[3])
+        self.plotpanel.setlim() 
+         
     def OnPress(self, xy):
-        print xy
         iter = 200
         trajectory=True
+        #self.mapsystem.Psetting = [(True,1.0), (False, 1.0)]
         self.mapsystem.setTrajectory(trajectory)
         self.mapsystem.setInit(maps.MapSystem.Point(self.mapsystem.dim,data=[xy[0],xy[1]]))
         self.mapsystem.evolves(iter)
-        xy=numpy.array(self.mapsystem.Trajectory[0]).transpose() if self.trajectory else numpy.array(self.mapsystem.P.data).transpose()[0]
-        self.plotpanel.replot(xy[0],xy[1],',')
+        xy=numpy.array(self.mapsystem.Trajectory[0]).transpose()
+        self.cmap_data.append(xy)
+        self.plotpanel.replot(xy[0],xy[1],',k')
         self.plotpanel.draw()
+        
     def get_range(self):
         qmin = float(wx.xrc.XRCCTRL(self.panel, "TextCtrlQmin").GetValue())
         qmax = float(wx.xrc.XRCCTRL(self.panel, "TextCtrlQmax").GetValue())
@@ -133,6 +198,7 @@ class QuantumPanel(_SubPanel):
         pmax = float(wx.xrc.XRCCTRL(self.panel, "TextCtrlPmax").GetValue())
         hdim = int(wx.xrc.XRCCTRL(self.panel, "TextCtrlHdim").GetValue())
         return (qmin, qmax, pmin, pmax, hdim)
+    
     def get_vrange(self):
         vqmin = float(wx.xrc.XRCCTRL(self.panel, "TextCtrlVQmin").GetValue())
         vqmax = float(wx.xrc.XRCCTRL(self.panel, "TextCtrlVQmax").GetValue())
@@ -141,32 +207,80 @@ class QuantumPanel(_SubPanel):
         col = int(wx.xrc.XRCCTRL(self.panel, "TextCtrlQgrid").GetValue())
         row = int(wx.xrc.XRCCTRL(self.panel, "TextCtrlPgrid").GetValue())
         return (vqmin, vqmax, vpmin, vpmax,col, row)
+    
     def get_initial(self):
         q_c = float(wx.xrc.XRCCTRL(self.panel, "TextCtrlq_c").GetValue())
         p_c = float(wx.xrc.XRCCTRL(self.panel, "TextCtrlp_c").GetValue())
         return (q_c, p_c)
-    def Draw(self, iter):
+
+    def DrawHsm(self, iter, rep='hsm'):
         range = self.get_vrange()
         xmin, xmax = range[0],range[1]
         ymin, ymax = range[2],range[3]
         col, row = range[4], range[5]
         self.qmap.setVRange(xmin, xmax, ymin, ymax, col, row)
+        self.set_lim()
+
+        x = numpy.arange(xmin, xmax, (xmax - xmin)/col)
+        y = numpy.arange(ymin, ymax, (ymax - ymin)/row)
+        X,Y = numpy.meshgrid(x,y)
         if iter == 0:
             data = self.qmap.get_hsmdata(self.qmap.ivec)
         else:
             data = self.qmap.get_hsmdata(self.qmap.qvecs[iter])
-        x = numpy.arange(xmin, xmax, (xmax - xmin)/col)
-        y = numpy.arange(ymin, ymax, (ymax - ymin)/row)
-        X,Y = numpy.meshgrid(x,y)
-        #todo
-        # I want to use imshow (for view), but ... 
-        #extent = (xmin, xmax, ymin, ymax)
-        self.plotpanel.plot()
+        
+        self.plotpanel.clear()
+        import pylab
         if self.logplot:
-            self.plotpanel.axes.contourf(X,Y,numpy.log(data), hold='on', color='k')
+            data = self.qmap.rounding_hsmdata(data, 1.0/self.vmin)
+            index = numpy.where(data == 0.0)
+            data[index] = numpy.nan
+            self.plotpanel.axes.contourf(X,Y,numpy.log(data),color='k')
+            self.plotpanel.axes.contourf(X,Y,numpy.log(data), cmap=pylab.cm.Oranges)
         else:
-            self.plotpanel.axes.contourf(X,Y,data, hold='on', color='k')
-        #self.plotpanel.axes.imshow(data,cmap=pylab.cm.Oranges,extent=extent)
+            #index = numpy.where(data < 1e-4)
+            #data[index] = numpy.nan
+            self.plotpanel.axes.contour(X,Y,data,color='k')
+            self.plotpanel.axes.contourf(X,Y,data,cmap=pylab.cm.Oranges)
+        if True:
+            for xy in self.cmap_data:
+                self.plotpanel.replot(xy[0],xy[1],',k')
+        self.plotpanel.setlim()
         self.plotpanel.draw()
+        
+    def Draw(self, iter, rep):
+        if rep in ('hsm'): raise TypeError
+        range = self.get_range()
+        vrange = self.get_vrange()
+        qmin, qmax = range[0], range[1]
+        pmin, pmax = range[2], range[3]
+        hdim = range[4]
+        self.plotpanel.plot()
+        
+        q = numpy.arange(qmin, qmax, (qmax - qmin)/hdim)
+        p = numpy.arange(pmin, pmax, (pmax - pmin)/hdim)
+
+        
+        if self.logplot: self.plotpanel.ylim = (self.vmin, None)
+        else: self.plotpanel.ylim = (0, None)
+
+        if rep == 'q':
+            self.plotpanel.xlim = (vrange[0], vrange[1])
+            self.plotpanel.setlim()
+            data = self.qmap.qvecs[iter]
+            
+            self.plotpanel.plot(q, numpy.abs(data)**2,'-')
+            
+        elif rep == 'p':
+            self.plotpanel.xlim = (vrange[2],vrange[3])
+            self.plotpanel.setlim()
+            data = self.qmap.pvecs[iter]
+            
+            self.plotpanel.plot(p, numpy.abs(data)**2,'-')
+        if self.logplot: self.plotpanel.axes.set_yscale('log')
+        else : self.plotpanel.axes.set_yscale('linear')
+        #else self.logplot:
+        self.plotpanel.draw()
+        
 
         
